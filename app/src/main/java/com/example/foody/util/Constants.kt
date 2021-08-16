8949d9c6bb4a45eb52c1658b79889ab44ebb5ba4
NandaGopal <ngopal253@gmail.com>
2021-08-16 21:03:04 +0530

    adapters-setup

--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -2,7 +2,7 @@ package com.example.foody.util
 
 class Constants {
     companion object {
-        const val API_Key = ""
+        const val API_KEY = ""
         const val BASE_URL = "https://api.spoonacular.com"
     }
 }
